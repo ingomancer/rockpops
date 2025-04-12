f387546d81bcdc997ff79f3aba56862251441b7a
Ingo Junefjord <lord.ingo@gmail.com>
2025-04-12 16:10:13 +0200

    Add rock paper shuffle mechanic

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,49 @@
+use std::time::Instant;
+
 use macroquad::{miniquad::window::screen_size, prelude::*};
 
-fn initial_state() -> (Circle, Rect, Circle, i32, bool) {
-    let player = Circle::new(50.0, 90.0, 20.0);
-    let paper = Rect {
-        x: 200.0,
-        y: 200.0,
-        w: 35.0,
-        h: 35.0,
-    };
-    let scissor = Circle::new(300.0, 300.0, 25.0);
-    let score = 0;
-    (player, paper, scissor, score, false)
+fn initial_state() -> (Vec<Circle>, i32, bool) {
+    (
+        vec![
+            Circle::new(50.0, 90.0, 20.0),
+            Circle {
+                x: 200.0,
+                y: 200.0,
+                r: 20.0,
+            },
+            Circle::new(300.0, 300.0, 20.0),
+        ],
+        0,
+        false,
+    )
 }
 
 #[macroquad::main("rockpops")]
 async fn main() {
-    let (mut player, mut paper, mut scissor, mut score, mut game_started) = initial_state();
-    let player_speed = 4.0;
-    let paper_speed = 2.0;
+    let mut names = [
+        "rock".to_string(),
+        "paper".to_string(),
+        "scissors".to_string(),
+    ];
+    let mut colors = [WHITE, RED, GREEN];
+    let mut rotate_enabled = false;
+    let (mut actors, mut score, mut game_started) = initial_state();
+    let pspeed = 4.0;
+    let papseed = 2.0;
+    let mut start_time = Instant::now();
 
     loop {
         let (screen_width, screen_height) = screen_size();
         clear_background(BLACK);
         draw_text(
-            "You are a rock. Use WASD or arrows to move.",
+            &format!("You are a {}. Use WASD or arrows to move.", names[0]),
             10.0,
             10.0,
             20.0,
             WHITE,
         );
         draw_text(
-            "Crush scissors but don't get caught by papers!",
+            &format!("Crush {} but don't get caught by {}!", names[2], names[1]),
             10.0,
             30.0,
             20.0,
@@ -38,62 +51,87 @@ async fn main() {
         );
 
         if game_started {
-            draw_text(&format!("Score: {score}"), 10.0, 50.0, 20.0, WHITE);
+            draw_text(
+                &format!(
+                    "Score: {score}, playtime: {}",
+                    start_time.elapsed().as_secs()
+                ),
+                10.0,
+                50.0,
+                20.0,
+                WHITE,
+            );
 
-            player = move_player(player, player_speed);
-            player = Circle {
-                x: player.x.clamp(0.0 + player.r, screen_width - player.r),
-                y: player.y.clamp(0.0 + player.r, screen_height - player.r),
-                r: player.r,
+            actors[0] = move_player(actors[0], pspeed);
+            actors[0] = Circle {
+                x: actors[0]
+                    .x
+                    .clamp(0.0 + actors[0].r, screen_width - actors[0].r),
+                y: actors[0]
+                    .y
+                    .clamp(0.0 + actors[0].r, screen_height - actors[0].r),
+                r: actors[0].r,
             };
 
-            paper = follow_player(paper, player, paper_speed);
+            actors[1] = follow_player(actors[1], actors[0], papseed);
 
-            if player.overlaps(&scissor) {
-                scissor.x = rand::gen_range(0.0 + scissor.r, screen_width - scissor.r);
-                scissor.y = rand::gen_range(0.0 + scissor.r, screen_height - scissor.r);
+            if actors[0].overlaps(&actors[2]) {
+                actors[2].x = rand::gen_range(0.0 + actors[2].r, screen_width - actors[2].r);
+                actors[2].y = rand::gen_range(0.0 + actors[2].r, screen_height - actors[2].r);
                 score += 1;
             } else {
-                scissor.x += rand::gen_range(-1.0, 1.0);
-                scissor.y += rand::gen_range(-1.0, 1.0);
+                actors[2].x += rand::gen_range(-1.0, 1.0);
+                actors[2].y += rand::gen_range(-1.0, 1.0);
             }
 
-            if player.overlaps_rect(&paper) {
-                (player, paper, scissor, score, game_started) = initial_state();
+            if actors[0].overlaps(&actors[1]) {
+                (actors, score, game_started) = initial_state();
+            }
+
+            if rotate_enabled && start_time.elapsed().as_secs() % 10 == 0 {
+                actors.rotate_left(1);
+                names.rotate_left(1);
+                colors.rotate_left(1);
+                rotate_enabled = false;
+            }
+
+            if (start_time.elapsed().as_secs() + 1) % 10 == 0 {
+                rotate_enabled = true;
             }
         } else {
             draw_text("Press space to start", 10.0, 50.0, 20.0, WHITE);
             if is_key_down(KeyCode::Space) {
                 game_started = true;
+                start_time = Instant::now();
             }
         }
-        draw_hexagon(scissor.x, scissor.y, scissor.r, 2.0, false, RED, GREEN);
-        draw_circle(player.x, player.y, player.r, WHITE);
-        draw_rectangle(paper.x, paper.y, paper.w, paper.h, RED);
+        draw_circle(actors[0].x, actors[0].y, actors[0].r, colors[0]);
+        draw_circle(actors[1].x, actors[1].y, actors[1].r, colors[1]);
+        draw_circle(actors[2].x, actors[2].y, actors[2].r, colors[2]);
 
         next_frame().await;
     }
 }
 
-fn move_player(player: Circle, player_speed: f32) -> Circle {
+fn move_player(player: Circle, pspeed: f32) -> Circle {
     let mut move_y = 0.0;
     let mut move_x = 0.0;
     if is_key_down(KeyCode::W) || is_key_down(KeyCode::Up) {
-        move_y -= player_speed;
+        move_y -= pspeed;
     }
     if is_key_down(KeyCode::S) || is_key_down(KeyCode::Down) {
-        move_y += player_speed;
+        move_y += pspeed;
     }
     if is_key_down(KeyCode::A) || is_key_down(KeyCode::Left) {
-        move_x -= player_speed;
+        move_x -= pspeed;
     }
     if is_key_down(KeyCode::D) || is_key_down(KeyCode::Right) {
-        move_x += player_speed;
+        move_x += pspeed;
     }
     let distance = move_x.hypot(move_y);
     if distance > 0.0 {
-        move_x = (move_x / distance) * player_speed;
-        move_y = (move_y / distance) * player_speed;
+        move_x = (move_x / distance) * pspeed;
+        move_y = (move_y / distance) * pspeed;
     }
     Circle {
         x: player.x + move_x,
@@ -102,20 +140,19 @@ fn move_player(player: Circle, player_speed: f32) -> Circle {
     }
 }
 
-fn follow_player(paper: Rect, player: Circle, speed: f32) -> Rect {
-    let dx = player.x - paper.x;
-    let dy = player.y - paper.y;
+fn follow_player(follower: Circle, player: Circle, speed: f32) -> Circle {
+    let dx = player.x - follower.x;
+    let dy = player.y - follower.y;
     let distance = dx.hypot(dy);
     if distance > 0.0 {
         let move_x = (dx / distance) * speed;
         let move_y = (dy / distance) * speed;
-        Rect {
-            x: paper.x + move_x,
-            y: paper.y + move_y,
-            w: paper.w,
-            h: paper.h,
+        Circle {
+            x: follower.x + move_x,
+            y: follower.y + move_y,
+            r: follower.r,
         }
     } else {
-        paper
+        follower
     }
 }
